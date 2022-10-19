4207662b319a8c251286fadcabba8a61f962ce5a
alexeyeva23 <korolev.2002lexa@list.ru>
2022-10-19 08:56:08 +0300

    Initial Commit

---
+++ lection2022/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lection2022
+//
+//  Created by Yeva on 19.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lection2022/lection2022App.swift
@@ -0,0 +1,17 @@
+//
+//  lection2022App.swift
+//  lection2022
+//
+//  Created by Yeva on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct lection2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
